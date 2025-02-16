de1691b78edaa8f5f96c4bcea46a498a3dbdca74
devp-with-V <vedant02ms@gmail.com>
2025-02-16 23:17:30 +0530

    second

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("videoInput").addEventListener("change", function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        const videoURL = URL.createObjectURL(file);
-        document.getElementById("videoPlayer").src = videoURL;
-    }
-});
